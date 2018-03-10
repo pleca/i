2a53ec62b8335b6da7d04a06d4cb741b9c9052d4
sruj <chryplewiczpawel@wp.pl>
2018-03-10 11:47:38 +0100

    form documents

--- src/AppBundle/Form/docType.php
+++ src/AppBundle/Form/docType.php
@@ -9,12 +9,14 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\IntraDocuments;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Entity\IntraDocumentCategory;
 
 class docType extends AbstractType
 {
@@ -25,6 +27,7 @@ class docType extends AbstractType
             // ...
             ->add('documentDesc', TextareaType::class, array('label' => 'Opis', 'required' => false, 'attr' => array('class' => 'tinymce')))
             ->add('DocumentFile', FileType::class, array('label' => 'Plik'))
+            ->add('category', EntityType::class, array( 'class' => IntraDocumentCategory::class))
             ->add('submit', SubmitType::class, array('label' => 'Dodaj'))
             // ...
         ;
